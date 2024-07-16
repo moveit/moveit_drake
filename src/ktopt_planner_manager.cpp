ad817c26f72dcf254d864301eb3c8c0d1bd20e7c
Sebastian Jahr <sebastian.jahr@picknik.ai>
2024-07-16 11:59:52 +0200

    Get robot description working

--- src/ktopt_planner_manager.cpp
+++ src/ktopt_planner_manager.cpp
@@ -29,26 +29,29 @@ public:
   {
     robot_model_ = model;
     node_ = node;
-    parameter_namespace_ = parameter_namespace;
     param_listener_ = std::make_shared<ktopt_interface::ParamListener>(node, parameter_namespace);
-    //robot_description_subscriber_ = node_->create_subscription<std_msgs::msg::String>(
-    //    "robot_description", 10, std::bind(&KTOptPlannerManager::robot_description_callback, this, _1));
+
+
+  // set QoS to transient local to get messages that have already been published
+  // (if robot state publisher starts before planner)
     robot_description_subscriber_ = node_->create_subscription<std_msgs::msg::String>(
-      "/robot_description", rclcpp::SystemDefaultsQoS(), [this](const std_msgs::msg::String::SharedPtr msg){
-        if (!description_set_)
+      "robot_description", rclcpp::QoS(1).transient_local(), [this](const std_msgs::msg::String::SharedPtr msg){
+        if (robot_description_.empty())
         {
           robot_description_ = msg->data;
-          description_set_ = true;
           RCLCPP_INFO(getLogger(), "Robot description set");
         }
       });
-    description_set_ = false;
     RCLCPP_INFO(getLogger(), "KTOpt planner manager initialized!");
     return true;
   }
 
   bool canServiceRequest(const planning_interface::MotionPlanRequest& req) const override
   {
+    if(robot_description_.empty()){
+      RCLCPP_ERROR(getLogger(), "Robot description is empty, do you have a robot state publisher running?");
+      return false;
+    }
     if (req.goal_constraints.empty())
     {
       RCLCPP_ERROR(getLogger(), "Invalid goal constraints");
@@ -61,38 +64,7 @@ public:
       return false;
     }
 
-    // if (!description_set_)
-    // {
-    //   RCLCPP_ERROR_STREAM(getLogger(), "Robot description has not yet been set, " 
-    //                << "this means that Drake's internal MultibodyPlant "
-    //                << "and SceneGraph have not been initialised.");
-    //   // DEBG: Created a separate function to re-subscribe to the
-    //   // robot_description topic, but cannot execute it as this funciton itself
-    //   // is a const function, cannot modify the object from which it is called.
-    //   // Any function that is called inside as well will not be able to change
-    //   // the state of the object.
-
-    //   // initializeRobotDescription();
-    //   return false;
-    // }
     return true;
-  }
-
-  void initializeRobotDescription()
-  {
-    RCLCPP_INFO_STREAM(getLogger(), "Reinitializing subscription to robot description");
-    robot_description_subscriber_ = node_->create_subscription<std_msgs::msg::String>(
-      "/robot_description", rclcpp::SystemDefaultsQoS(), [this](const std_msgs::msg::String::SharedPtr msg)
-      {
-        if (!description_set_)
-        {
-          robot_description_ = msg->data;
-          description_set_ = true;
-          RCLCPP_INFO(getLogger(), "Robot description set");
-        }
-      });
-
-    return;
   }
 
   std::string getDescription() const override
@@ -131,24 +103,12 @@ public:
 private:
   moveit::core::RobotModelConstPtr robot_model_;
   rclcpp::Node::SharedPtr node_;
-  std::string parameter_namespace_;
   std::shared_ptr<ktopt_interface::ParamListener> param_listener_;
 
   // robot description related variables
   rclcpp::Subscription<std_msgs::msg::String>::SharedPtr robot_description_subscriber_;
   bool description_set_;
   std::string robot_description_;
-
-  void robot_description_callback(const std_msgs::msg::String::SharedPtr msg)
-  {
-    RCLCPP_INFO(getLogger(), "Retrieved robot description!!");
-    if (!description_set_)
-    {
-      robot_description_ = msg->data;
-      description_set_ = true;
-      RCLCPP_INFO_STREAM(getLogger(), "Robot description has been set.");
-    }
-  }
 };
 
 }  // namespace ktopt_interface
