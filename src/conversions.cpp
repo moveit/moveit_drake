a3df24f2ecf1c74f437e05e9f9bd80a3221a947a
Sebastian Jahr <sebastian.jahr@picknik.ai>
2024-08-21 22:12:09 +0200

    Add conversions source file

--- src/conversions.cpp
+++ src/conversions.cpp
@@ -33,19 +33,9 @@
  *********************************************************************/
 
 /* Author: Sebastian Jahr
-   Desc: TODO
 */
 
-#include <moveit/planning_scene/planning_scene.h>
-#include <moveit/robot_trajectory/robot_trajectory.h>
-
-#include <drake/multibody/parsing/parser.h>
-#include <drake/geometry/scene_graph.h>
-#include <drake/systems/framework/diagram.h>
-#include <drake/systems/framework/diagram_builder.h>
-#include <drake/multibody/plant/multibody_plant.h>
-#include <drake/common/trajectories/trajectory.h>
-#include <drake/common/trajectories/piecewise_polynomial.h>
+#include <moveit/drake/conversions.hpp>
 namespace moveit::drake
 {
 
@@ -55,26 +45,52 @@ using ::drake::multibody::MultibodyPlant;
 using ::drake::multibody::Parser;
 using ::drake::systems::DiagramBuilder;
 
-/**
- * @brief Create a Piecewise Polynomial from a moveit trajectory (see
- * https://drake.mit.edu/doxygen_cxx/classdrake_1_1trajectories_1_1_piecewise_polynomial.html)
- *
- * @param robot_trajectory MoveIt trajectory to be translated
- * @param group Joint group for which a piecewise polynomial is created
- * @return ::drake::trajectories::PiecewisePolynomial<double>
- */
 [[nodiscard]] ::drake::trajectories::PiecewisePolynomial<double>
 getPiecewisePolynomial(const ::robot_trajectory::RobotTrajectory& robot_trajectory,
-                       const moveit::core::JointModelGroup* group);
+                       const moveit::core::JointModelGroup* group)
+{
+  std::vector<double> breaks;
+  breaks.reserve(robot_trajectory.getWayPointCount());
+  std::vector<Eigen::MatrixXd> samples;
+  samples.reserve(robot_trajectory.getWayPointCount());
 
-/**
- * @brief Create a moveit trajectory from a piecewise polynomial. Assumes that the piecewise polynomial describes a
- * joint trajectory for every active joint of the given trajectory.
- *
- * @param drake_trajectory Drake trajectory
- * @param delta_t Time step size
- * @param moveit_trajectory MoveIt trajectory to be populated based on the piecewise polynomial
- */
+  // Create samples & breaks
+  for (std::size_t i = 0; i < robot_trajectory.getWayPointCount(); ++i)
+  {
+    const auto& state = robot_trajectory.getWayPoint(i);
+    Eigen::VectorXd position(state.getVariableCount());
+    state.copyJointGroupPositions(group, position);
+    samples.emplace_back(position);
+    breaks.emplace_back(robot_trajectory.getWayPointDurationFromStart(i));
+  }
+
+  // Create a piecewise polynomial trajectory
+  return ::drake::trajectories::PiecewisePolynomial<double>::FirstOrderHold(breaks, samples);
+}
+
 void getRobotTrajectory(const ::drake::trajectories::Trajectory<double>& drake_trajectory, const double delta_t,
-                        std::shared_ptr<::robot_trajectory::RobotTrajectory>& moveit_trajectory);
+                        std::shared_ptr<::robot_trajectory::RobotTrajectory>& moveit_trajectory)
+{
+  // Get the start and end times of the piecewise polynomial
+  double t_prev = 0.0;
+  const auto num_pts = static_cast<size_t>(std::ceil(drake_trajectory.end_time() / delta_t) + 1);
+
+  for (unsigned int i = 0; i < num_pts; ++i)
+  {
+    const auto t_scale = static_cast<double>(i) / static_cast<double>(num_pts - 1);
+    const auto t = std::min(t_scale, 1.0) * drake_trajectory.end_time();
+    const auto pos_val = drake_trajectory.value(t);
+    const auto vel_val = drake_trajectory.EvalDerivative(t);
+    const auto waypoint = std::make_shared<moveit::core::RobotState>(moveit_trajectory->getRobotModel());
+    const auto active_joints = moveit_trajectory->getRobotModel()->getActiveJointModels();
+    for (size_t joint_index = 0; joint_index < active_joints.size(); ++joint_index)
+    {
+      waypoint->setJointPositions(active_joints[joint_index], &pos_val(joint_index));
+      waypoint->setJointVelocities(active_joints[joint_index], &vel_val(joint_index));
+    }
+
+    moveit_trajectory->addSuffixWayPoint(waypoint, t - t_prev);
+    t_prev = t;
+  }
+}
 }  // namespace moveit::drake
